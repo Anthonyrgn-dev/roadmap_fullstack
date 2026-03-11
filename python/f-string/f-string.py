82c2851fe89edae42db9de90bd6a9857f3b3059f
Anthony <ringeisen.anthony@icloud.com>
2026-03-11 15:27:46 +0100

    f-string

---
+++ python/f-string/f-string.py
@@ -0,0 +1,12 @@
+prenom = "Anthony"
+phrase = f"Bonjour {prenom}"
+
+print(phrase)
+
+protocole = "https://"
+nom_du_site = "docstring"
+extension = "fr"
+page = "glossaire"
+
+URL = f"{protocole}www.{nom_du_site}.{extension}/{page}/"
+print(URL)
