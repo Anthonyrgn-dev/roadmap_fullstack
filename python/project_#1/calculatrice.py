22e625fcc43921fd8c13e2e7a50bef0b1d04e4c3
Anthony <ringeisen.anthony@icloud.com>
2026-03-13 18:25:20 +0100

    first calculatrice project

---
+++ python/project_#1/calculatrice.py
@@ -0,0 +1,6 @@
+first_number = input("Veuillez entrer un premier nombre : ")
+second_number = input("Veuillez entrer un deuxieme nombre : ")
+
+addition = int(first_number) + int(second_number)
+
+print(f"Le resultat de l'addition du nombre {first_number} avec {second_number} est egal a {addition}")
